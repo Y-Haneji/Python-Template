706596c04514e07f7343b81b7c99b14f4a71a0f6
Haneji Yuto <49736402+Y-Haneji@users.noreply.github.com>
2023-01-03 21:52:25 +0900

    hydra用のユーティリティ

---
+++ hydra_compose.py
@@ -0,0 +1,16 @@
+from typing import List
+import os
+from hydra import compose, initialize
+from omegaconf import OmegaConf
+
+
+def get_cnf(config_path, overrides: List[str] = []):
+    """
+    設定値の辞書を取得
+    @return
+        cnf: OmegaDict
+    """
+    with initialize(config_path=os.path.dirname(config_path), version_base=None): # 1.2では、version_base=Noneという引数も必要になっていた
+        cnf = compose(config_name=os.path.basename(config_path), overrides=overrides)
+        print(OmegaConf.to_yaml(cnf))
+        return cnf
