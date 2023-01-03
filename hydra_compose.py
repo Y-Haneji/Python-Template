3de076ba3bee69a3a6af7acb5c9d5bbbb2cbdafa
Haneji Yuto <49736402+Y-Haneji@users.noreply.github.com>
2023-01-03 21:56:18 +0900

    構成後のconfigを表示するかのフラグの引数を追加

--- hydra_compose.py
+++ hydra_compose.py
@@ -4,7 +4,7 @@ from hydra import compose, initialize
 from omegaconf import OmegaConf
 
 
-def get_cnf(config_path, overrides: List[str] = []):
+def get_cnf(config_path, overrides: List[str] = [], verbose=False):
     """
     設定値の辞書を取得
     @return
@@ -12,5 +12,6 @@ def get_cnf(config_path, overrides: List[str] = []):
     """
     with initialize(config_path=os.path.dirname(config_path), version_base=None): # 1.2では、version_base=Noneという引数も必要になっていた
         cnf = compose(config_name=os.path.basename(config_path), overrides=overrides)
-        print(OmegaConf.to_yaml(cnf))
+        if verbose:
+            print(OmegaConf.to_yaml(cnf))
         return cnf
